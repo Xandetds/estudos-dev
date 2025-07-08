72c9deaed78b576e40197ad1c4523547e8385af0
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-07 21:36:25 -0300

    feat: Adicionado exercício Contador de Letras

---
+++ python/exercicio04.py
@@ -0,0 +1,30 @@
+#  Exercício 04 – Contagem de letras (sem acentos e sem espaços)
+#
+# Crie uma função em Python chamada contar_letras que recebe uma frase (string)
+# como parâmetro e retorna um dicionário com a quantidade de vezes que cada letra
+# aparece na frase.
+#
+#  Regras:
+# - Ignorar maiúsculas/minúsculas (deixar tudo minúsculo).
+# - Ignorar espaços, números e pontuação.
+# - Contar somente as letras do alfabeto (de A a Z).
+
+import string
+
+frase = input("Digite uma frase: ")
+
+def contar_letras(frase):
+    frase = frase.lower()
+    letras = {}
+    
+    for caractere in frase:
+        if caractere in string.ascii_lowercase: 
+            if caractere in letras:
+                letras[caractere] += 1
+            else:
+                letras[caractere] = 1
+    return letras
+
+print(contar_letras(frase))
+            
+
