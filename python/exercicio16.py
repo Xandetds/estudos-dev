6c4d9561ae456fc07d2ae9e84753e10f452223fa
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-09 20:25:50 -0300

    feat: Adicionado exercício de Verificador de Ano Bissexto

--- python/exercicio16.py
+++ python/exercicio16.py
@@ -5,6 +5,19 @@
 #
 # **Regras:**
 # - Um ano é bissexto se for divisível por 4.
-# - EXCEÇÃO: Anos divisíveis por 100 NÃO são bissextos, a menos que...
-# - EXCEÇÃO DA EXCEÇÃO: ...sejam divisíveis por 400.
+# - EXCEÇÃO: Anos divisíveis por 100 NÃO são bissextos, a menos que sejam divisíveis por 400.
 
+
+def eh_ano_bissexto(ano):
+    if ano % 400 == 0:
+        return True
+    elif ano % 100 == 0:
+        return False
+    elif ano % 4 == 0:
+        return True
+    else:
+        return False
+
+ano_usuario = int(input("Digite um ano: "))
+resultado = eh_ano_bissexto(ano_usuario)
+print(resultado)
