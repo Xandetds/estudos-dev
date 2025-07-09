672828b8cfccfcaee7a8adb39a8bade365351216
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-09 14:40:36 -0300

    feat: Adicionado exercício que conta tipos de caracteres

--- python/exercicio13.py
+++ python/exercicio13.py
@@ -4,3 +4,25 @@
 # - Letras minúsculas
 # - Números
 # - Especiais (!@#$%&*?, etc.)
+
+def tipos_de_caracteres(texto):
+    contagem = {'especiais': 0, 'numeros': 0, 'maiusculas': 0, 'minusculas': 0}
+    especiais = '!@#$%&.,;:(){}[]*?'
+
+    for caractere in texto:
+        if caractere in especiais:
+            contagem['especiais'] += 1
+        elif caractere.isdigit():
+            contagem['numeros'] += 1
+        elif caractere.isupper():
+            contagem['maiusculas'] += 1
+        elif caractere.islower():
+            contagem['minusculas'] += 1
+
+    return contagem
+
+
+texto_usuario = input("Digite um texto: ")
+resultado = tipos_de_caracteres(texto_usuario)
+print(resultado)
+ 
