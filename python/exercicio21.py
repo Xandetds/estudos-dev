fe4e9e68309df88df163369bc8dd26f659442431
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-10 20:09:58 -0300

    feat: Adicionado exercício de Agrupar Elementos por Critério

--- python/exercicio21.py
+++ python/exercicio21.py
@@ -5,3 +5,22 @@
 # 'pares' -> contendo uma lista de todos os números pares da lista original.
 # 'impares' -> contendo uma lista de todos os números ímpares da lista original.
 
+def agrupar_por_paridade(lista):
+    pares = []
+    impares = []
+    dicionario = {'pares': pares, 'impares': impares}
+    for numero in lista:
+        if numero % 2 == 0:
+            pares.append(numero)
+        else:
+            impares.append(numero)
+    return dicionario
+
+lista_usuario = input("Digite uma lista de números inteiros, separadas só por espaços: ")
+lista_em_int = list(map(int, lista_usuario.split()))
+resultado = agrupar_por_paridade(lista_em_int)
+print(resultado)
+
+
+    
+    
