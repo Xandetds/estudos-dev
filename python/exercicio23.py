b572cf40913a44692f0549c99578126b2bb95296
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-10 21:34:14 -0300

    feat: Adicionado exercício de Encontrar Segundo Maior Número

--- python/exercicio23.py
+++ python/exercicio23.py
@@ -7,3 +7,21 @@
 # - A lista terá sempre pelo menos dois números.
 # - Não use a função `sort()` ou `sorted()` do Python. Faça usando lógica de comparação.
 # - Os números na lista podem ser repetidos.
+
+def encontrar_segundo_maior(lista):
+    maior = float('-inf')
+    segundo_maior = float('-inf')
+
+    for numero in lista:
+        if numero > maior:
+            segundo_maior = maior
+            maior = numero
+        elif numero > segundo_maior and numero != maior:
+            segundo_maior = numero
+
+    return segundo_maior
+
+entrada = input("Digite uma lista de números separados por espaço: ")
+lista_usuario = list(map(int, entrada.split()))
+resultado = encontrar_segundo_maior(lista_usuario)
+print(resultado)
