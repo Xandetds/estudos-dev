f316bc7482d74802218f7bfb970ced213ffb6db0
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-11 19:27:26 -0300

    feat: Adicionado exercício de Contar Vogais e Consoantes em uma Palavra

--- python/exercicio25.py
+++ python/exercicio25.py
@@ -7,3 +7,20 @@
 # - Considere as vogais: 'a', 'e', 'i', 'o', 'u' (case-insensitive).
 # - Tudo que não for vogal (e for letra) é consoante.
 # - Ignore números, espaços e caracteres especiais.
+
+def contar_vogais_consoantes_palavra(texto):
+    contagem = {'vogais': 0, 'consoantes': 0}
+    vogais = 'aeiou'
+    consoantes = 'bcdfghjklmnpqrstvwxyz'
+    for letra in texto:
+        letra = letra.lower()   
+        if letra in vogais:
+            contagem['vogais'] += 1
+        elif letra in consoantes:
+            contagem['consoantes'] += 1
+    return contagem
+
+
+texto_usuario = input("Digite um texto: ")
+resultado = contar_vogais_consoantes_palavra(texto_usuario)
+print(resultado)
