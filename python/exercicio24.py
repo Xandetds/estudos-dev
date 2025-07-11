17f264144a2ff8b0c176410678d9615d66e2c6c6
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-11 18:49:16 -0300

    feat: Adicionado exercício de Remover Caracteres Específicos

--- python/exercicio24.py
+++ python/exercicio24.py
@@ -3,3 +3,17 @@
 #
 # A função deve retornar uma nova string com todos os caracteres
 # presentes em `caracteres_para_remover` removidos do `texto` original.
+
+
+def remover_caracteres(texto, caracteres_para_remover):
+    caracteres_removidos = ''
+    for caractere in texto:
+        if caractere not in caracteres_para_remover:
+            caracteres_removidos += caractere
+    return caracteres_removidos
+
+texto_usuario = input('Digite um texto: ')
+caracteres_para_remover_usuario = input('Digite os caracteres que quer remover do texto: ')
+resultado = remover_caracteres(texto_usuario, caracteres_para_remover_usuario)
+print(resultado)
+                    
