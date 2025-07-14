f7c75e995e75661b738751d4a161d4d49f412282
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-14 10:01:57 -0300

    feat: Adicionado exercício de Combinar Palavras em Frase

--- python/exercicio31.py
+++ python/exercicio31.py
@@ -3,3 +3,15 @@
 #
 # A função deve retornar uma única string que é a concatenação de todas as strings,
 # separadas por um espaço.
+
+def combinar_palavras(*args):
+    return " ".join(args)
+
+string1 = input("Digite uma palavra: ")
+string2 = input("Digite outra palavra: ")
+string3 = input("Digite outra palavra: ")
+string4 = input("Digite outra palavra: ")
+string5 = input("Digite outra palavra: ")
+
+resultado = combinar_palavras(string1, string2, string3, string4, string5)
+print(resultado)
