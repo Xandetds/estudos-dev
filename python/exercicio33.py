2399f15192c505dd2ff1941c2fdcddf69dfbdee6
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-14 10:30:55 -0300

    feat: Adicionado exercício de Sortear Múltiplos Elementos Aleatórios

--- python/exercicio33.py
+++ python/exercicio33.py
@@ -7,3 +7,23 @@
 # **Regras:**
 # - A lista original não estará vazia.
 # - A `quantidade` será um número positivo.
+
+import random
+
+def sortear_varios_elementos(lista, quantidade):
+    elementos_sorteados = []
+    for _ in range(quantidade): 
+        elemento_escolhido = random.choice(lista) 
+        elementos_sorteados.append(elemento_escolhido) 
+
+    return elementos_sorteados 
+
+lista_usuario_string = input("Digite os elementos da lista (separados por vírgula e espaço, ex: a, b, c): ")
+lista_usuario = [item.strip() for item in lista_usuario_string.split(',')]
+
+sorteios_usuario = int(input("Quantos elementos você quer sortear? "))
+
+resultado = sortear_varios_elementos(lista_usuario, sorteios_usuario)
+
+print(resultado)
+    
