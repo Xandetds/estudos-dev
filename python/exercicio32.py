57eae4a21beb27dbeed1e247efbec6f9efa8918b
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-14 10:12:48 -0300

    feat: Adicionado exercício de Gerar Frase Personalizada

--- python/exercicio32.py
+++ python/exercicio32.py
@@ -4,3 +4,12 @@
 # A função deve retornar uma frase no formato:
 # "[Nome] [verbo] [objeto]."
 
+def gerar_frase_personalizada(nome, verbo, objeto):
+    return " ".join((nome, verbo, objeto))
+
+
+nome_usuario = input("Digite o nome (ex: O cachorro): ")
+verbo_usuario = input("Digite o verbo (ex: corre atrás): ")
+objeto_usuario = input("Digite o objeto (ex: da bola): ")
+resultado = gerar_frase_personalizada(nome_usuario, verbo_usuario, objeto_usuario)
+print(resultado)
