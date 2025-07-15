880e17797044ae5a807afa72ee57ee24ca20787d
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-15 20:02:27 -0300

    feat: Adicionado exercício de Operações Básicas com Números em JS

--- javascript/exercicio02.js
+++ javascript/exercicio02.js
@@ -10,3 +10,16 @@
 // Subtração: 5
 // Multiplicação: 50
 // Divisão: 2
+
+let numero1 = 1;
+let numero2 = 2;
+
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+console.log(`A soma de ${numero1} e de ${numero2} é ${soma}`);
+console.log(`A subtração de ${numero1} e de ${numero2} é ${subtracao}`);
+console.log(`A multiplicação de ${numero1} e de ${numero2} é ${multiplicacao}`);
+console.log(`A divisão de ${numero1} e de ${numero2} é ${divisao}`);
