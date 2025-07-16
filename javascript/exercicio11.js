d6bb11887471bcbc17bf8606e362cf090d92ab88
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-16 14:23:32 -0300

    feat: Adicionado exercício de Buscar Objeto por Propriedade em JS

--- javascript/exercicio11.js
+++ javascript/exercicio11.js
@@ -7,3 +7,25 @@
 //
 // **Regras:**
 // - A busca deve ser case-sensitive para o valor.
+
+
+function buscarObjetoPorPropriedade(arrayDeObjetos, chave, valor_buscado) {
+    for (let objetoAtual of arrayDeObjetos) {
+        if (objetoAtual.hasOwnProperty(chave) && objetoAtual[chave] === valor_buscado) {
+            return objetoAtual; 
+        }
+    }
+    return null;
+}
+
+let pessoas = [
+    { id: 1, nome: "joao", idade: 30 },
+    { id: 2, nome: "xande", idade: 25 },
+    { id: 3, nome: "joao", idade: 35 }
+];
+
+let pessoaEncontrada1 = buscarObjetoPorPropriedade(pessoas, 'nome', 'xande');
+console.log(pessoaEncontrada1);
+
+let pessoaEncontrada2 = buscarObjetoPorPropriedade(pessoas, 'idade', 35);
+console.log(pessoaEncontrada2);
