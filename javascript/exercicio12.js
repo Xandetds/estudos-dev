cc3df7a883da1f700130a6f25b066caa3f0abe92
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-16 15:15:17 -0300

    feat: Adicionado exercício de Contar Caracteres de um Tipo Específico em JS

--- javascript/exercicio12.js
+++ javascript/exercicio12.js
@@ -10,3 +10,46 @@
 // - Para 'espacos', conte ' '.
 // - Para 'especiais', conte caracteres que não são letras, números ou espaços.
 // - A função deve ser case-insensitive ao contar letras.
+
+
+function contarCaracteresTipo(texto, tipo) {
+    let contador = 0;
+    let textoMinusculo = texto.toLowerCase();
+    const caracteresEspeciaisPadrao = '!@#$%^&*()_+{}[]:;<>,.?/~`-="\'\\|';
+
+    for (let char of textoMinusculo) {
+        if (tipo === 'letras') {
+            if (char >= 'a' && char <= 'z') {
+                contador++;
+            }
+        } else if (tipo === 'numeros') {
+            if (char >= '0' && char <= '9') {
+                contador++;
+            }
+        } else if (tipo === 'espacos') {
+            if (char === ' ') {
+                contador++;
+            }
+        } else if (tipo === 'especiais') {
+            if (caracteresEspeciaisPadrao.includes(char) && !((char >= 'a' && char <= 'z') || (char >= '0' && char <= '9') || char === ' ')) {
+                contador++;
+            }
+        }
+    }
+    return contador;
+}
+
+
+let minhaString = "Olá Mundo 123! @#$ dasdasdasdasdsads";
+
+let contagemLetras = contarCaracteresTipo(minhaString, 'letras');
+console.log(contagemLetras);
+
+let contagemNumeros = contarCaracteresTipo(minhaString, 'numeros');
+console.log(contagemNumeros);
+
+let contagemEspacos = contarCaracteresTipo(minhaString, 'espacos');
+console.log(contagemEspacos);
+
+let contagemEspeciais = contarCaracteresTipo(minhaString, 'especiais');
+console.log(contagemEspeciais);
