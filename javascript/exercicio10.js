bb355a3dadd67f7297187220a8f06780a0a4eba9
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-16 11:35:16 -0300

    feat: Adicionado exercício de Combinar Arrays Sem Duplicatas em JS

--- javascript/exercicio10.js
+++ javascript/exercicio10.js
@@ -7,3 +7,29 @@
 //
 // **Regras:**
 // - Use loops e condicionais, não métodos como `Set` ou `filter` avançado para remover duplicatas.
+
+function combinarArraysUnicos(array1, array2) {
+    let arrayCombinadoTemporario = array1.concat(array2);
+
+    let arrayCombinado = []; 
+
+    for (let elemento of arrayCombinadoTemporario) {
+        if (!arrayCombinado.includes(elemento)) {
+            arrayCombinado.push(elemento); 
+        }
+    }
+
+    return arrayCombinado;
+}
+
+
+let numerosArray1 = [1, 2, 3, 2];
+let numerosArray2 = [3, 4, 5, 1];
+let resultadoNumeros = combinarArraysUnicos(numerosArray1, numerosArray2);
+console.log(`Arrays de números combinados e únicos: [${resultadoNumeros}]`); 
+
+
+let frutasArray1 = ["maçã", "banana", "uva"];
+let frutasArray2 = ["banana", "laranja", "maçã"];
+let resultadoFrutas = combinarArraysUnicos(frutasArray1, frutasArray2);
+console.log(`Arrays de frutas combinados e únicos: [${resultadoFrutas}]`);
