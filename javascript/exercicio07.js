f63df78e2ec174322c34b08f50ab74f062795a49
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-15 21:40:12 -0300

    feat: Adicionado exercício de Filtrar Elementos Pares de Array em JS

--- javascript/exercicio07.js
+++ javascript/exercicio07.js
@@ -7,3 +7,17 @@
 // **Regras:**
 // - O array original pode estar vazio.
 // - Use um loop para percorrer o array.
+
+function filtrarNumerosPares(numeros) {
+  let pares = [];
+
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0) {
+      pares.push(numeros[i]);
+    }
+  }
+
+  return pares;
+}
+
+console.log(filtrarNumerosPares([1, 2, 3, 4, 5, 6, 77, 88]))
