0efaf04510acc0cd08d7f626240f3c683df632af
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-15 21:25:04 -0300

    feat: Adicionado exercício de Função para Calcular Fatorial em JS

--- javascript/exercicio05.js
+++ javascript/exercicio05.js
@@ -8,3 +8,23 @@
 // **Regras:**
 // - O fatorial de 0 é 1.
 // - O fatorial de um número negativo não é definido para este exercício (pode ignorar ou retornar uma mensagem de erro).
+
+function calcularFatorial(n) {
+  if (n < 0) {
+    return "Fatorial não definido para número negativo";
+  } else if (n === 0) {
+    return 1;
+  } else {
+    let resultado = 1;
+    for (let i = 1; i <= n; i++) {
+      resultado *= i;
+    }
+    return resultado;
+  }
+}
+
+
+
+console.log(calcularFatorial(5)); 
+console.log(calcularFatorial(0)); 
+
