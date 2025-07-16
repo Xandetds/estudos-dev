a8448a98bef2a1b3234698184ee12cf8de67189a
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-15 21:16:49 -0300

    feat: Adicionado exercício de Verificação de Paridade em Loop em JS

--- javascript/exercicio04.js
+++ javascript/exercicio04.js
@@ -3,3 +3,7 @@
 // Crie um script que use um loop (for ou while) para iterar de 1 a 20.
 // Para cada número, imprima se ele é "Par" ou "Ímpar" no console.
 //
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 == 0) {console.log(`o numero ${i} é par.`);}
+  else {console.log(`o numero ${i} é impar`);}
+}
