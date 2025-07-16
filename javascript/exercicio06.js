2f861d642925b9cc43a612c5c38c35b707f8ac71
Alexandre Tibes <alexandretibes9@gmail.com>
2025-07-15 21:32:17 -0300

    feat: Adicionado exercício de Busca de Maior Valor em Array em JS

--- javascript/exercicio06.js
+++ javascript/exercicio06.js
@@ -10,3 +10,20 @@
 // - Não use o método `Math.max()` ou `.sort()` diretamente para encontrar o maior.
 //   Faça a lógica de comparação manualmente com um loop.
 //
+
+function encontrarMaiorNumero(numeros) {
+  let maior = numeros[0];  
+  for (let i = 1; i < numeros.length; i++) {
+    if (numeros[i] > maior) {
+      maior = numeros[i]; 
+    }
+  }
+
+  return maior;
+}
+
+
+let lista = [10, 45, 32, 87, 3];
+console.log(encontrarMaiorNumero(lista)); 
+
+console.log(encontrarMaiorNumero([1, 2, 3, 4, 5, 66]))
