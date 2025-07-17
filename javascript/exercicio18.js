7cb297a23b18356e3956d37d04b1b191059143ec
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-17 14:27:21 -0300

    feat: Adicionado exercício de Filtrar Palavras por Letra Inicial em JS

--- javascript/exercicio18.js
+++ javascript/exercicio18.js
@@ -8,3 +8,26 @@
 // **Regras:**
 // - A comparação deve ser case-insensitive.
 // - Se o array de palavras estiver vazio, retorne um array vazio.
+
+function filtrarPorLetraInicial(palavras, letraInicial) {
+    const palavrasFiltradas = [];
+
+    if (palavras.length === 0) {
+        return [];
+    }
+
+    const letraInicialLowerCase = letraInicial.toLowerCase();
+
+    for (let palavra of palavras) {
+        if (palavra && palavra.toLowerCase().startsWith(letraInicialLowerCase)) {
+            palavrasFiltradas.push(palavra);
+        }
+    }
+
+    return palavrasFiltradas;
+}
+
+console.log(filtrarPorLetraInicial(["Maçã", "Banana", "Morango", "Manga"], "m"));
+console.log(filtrarPorLetraInicial(["Cachorro", "Gato", "Coelho"], "g"));
+console.log(filtrarPorLetraInicial([], "a"));
+console.log(filtrarPorLetraInicial(["Abacaxi"], "A"));
