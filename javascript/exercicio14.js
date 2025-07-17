46883debcaa4cc73bc2488e1285229c22573025e
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-17 11:02:50 -0300

    feat: Adicionado exercício de Somar Elementos de Array em JS

--- javascript/exercicio14.js
+++ javascript/exercicio14.js
@@ -7,3 +7,21 @@
 // **Regras:**
 // - O array pode conter números positivos, negativos ou zero.
 // - O array pode estar vazio, neste caso, retorne 0.
+
+
+function somarArray(numeros){
+    if (numeros.length === 0) {
+        return 0; 
+    }
+    let soma = 0; 
+    for (let numero of numeros) {
+        soma += numero; // <-- Esta linha agora está DENTRO do loop e será executada para cada 'numero'.
+    }
+    return soma;
+}
+
+
+console.log(somarArray([1, 2, 3]));      
+console.log(somarArray([-1, 5, 0]));    
+console.log(somarArray([]));           
+console.log(somarArray([10]));
