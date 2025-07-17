10df2465700117f74c0f5ef99abc19a59906d732
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-17 10:43:26 -0300

    feat: Adicionado exercício de Contar Vogais em String em JS

--- javascript/exercicio13.js
+++ javascript/exercicio13.js
@@ -6,3 +6,20 @@
 //
 // **Regras:**
 // - A contagem deve ser case-insensitive (não diferenciar maiúsculas de minúsculas).
+
+
+function contarVogais(string){
+    let vogais = 'aeiouáéíóúàèìòùãõü'; 
+    let contador = 0;    
+    for (let letra of string) {
+        letra = letra.toLowerCase();
+        if (vogais.includes(letra)) { 
+            contador += 1; 
+        }
+    }
+    return contador; 
+}
+
+console.log(contarVogais("olá MUNDO")); 
+console.log(contarVogais("JavaScript")); 
+console.log(contarVogais("xande tibes"));
