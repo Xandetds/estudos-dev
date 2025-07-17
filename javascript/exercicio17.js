85fdfe94423d75a8f0698561f8d25787aa9ab474
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-17 14:27:07 -0300

    feat: Adicionado exercício de Calcular Média de Notas em JS

--- javascript/exercicio17.js
+++ javascript/exercicio17.js
@@ -6,3 +6,22 @@
 //
 // **Regras:**
 // - Se o array estiver vazio, retorne 0 para evitar divisão por zero.
+
+function calcularMediaNotas(notas) {
+    if (notas.length === 0) {
+        return 0;
+    }
+
+    let soma = 0;
+    for (let i = 0; i < notas.length; i++) {
+        soma += notas[i];
+    }
+
+    const media = soma / notas.length;
+    return media;
+}
+
+console.log(calcularMediaNotas([10, 20, 30]));
+console.log(calcularMediaNotas([70, 80, 90, 100]));
+console.log(calcularMediaNotas([]));
+console.log(calcularMediaNotas([50]));
