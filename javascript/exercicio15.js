e9d57cc3aa6f803380c8cf56c191444e7c3c80d3
Alexandre Tibes <Alexandretibes9@gmail.com>
2025-07-17 13:24:21 -0300

    feat: Adicionado exercício de Verificar Palíndromo Básico em JS

--- javascript/exercicio15.js
+++ javascript/exercicio15.js
@@ -7,3 +7,25 @@
 //
 // **Regras:**
 // - Considere apenas letras e números. Ignore espaços, pontuações e maiúsculas/minúsculas.
+
+function ehPalindromo(stringOriginal) { 
+    let stringLimpa = stringOriginal.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+    let inversa = '';
+    for (let i = stringLimpa.length - 1; i >= 0; i--) {
+        inversa += stringLimpa[i];
+    }
+    if (stringLimpa === inversa) {
+        return true; 
+    } else {
+        return false; 
+    }
+}
+
+let palavra1 = 'arara'
+let palavra2 = 'alexandre'
+let frase1 = 'Ame o poema';
+
+console.log(ehPalindromo(palavra1))
+console.log(ehPalindromo(palavra2))
+console.log(ehPalindromo(frase1))
