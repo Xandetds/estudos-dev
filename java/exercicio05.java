1c0cc957d6280f8c412329f428dd3259079c0ccf
Alexandre Tibes da Silva <alexandretibes9@gmail.com>
2025-07-27 15:45:23 -0300

    feat: Adicionado exercício de leitura de nomes e contagem de caracteres com interface

--- java/exercicio05.java
+++ java/exercicio05.java
@@ -6,3 +6,48 @@
 // - O programa deve exibir o número de caracteres de cada nome digitado.
 // - O programa deve encerrar imediatamente após o usuário digitar "fim".
 //
+
+import java.util.Scanner;
+
+
+
+interface Leitor{
+    String getNome(String nome);
+    void contarCaracteres(String nome);
+}
+
+class LerInformacoes implements Leitor{
+    @Override 
+    public String getNome(String nome){
+            return nome;
+    }
+
+    @Override
+    public void contarCaracteres(String getNome){
+     int caracteres = getNome.length();
+            System.out.println("O numero de caracteres no nome é: " + caracteres);
+}
+}
+
+
+public class exercicio05{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Leitor nomeLeitor = new LerInformacoes();
+
+        while(true){
+        System.out.println("Digite um nome: ");
+        String nome = scanner.nextLine();
+        if (nome.equals("fim")) {
+                System.out.println("Leitura encerrada, fim.");
+                break;
+            }
+            
+
+        nomeLeitor.getNome(nome);
+        nomeLeitor.contarCaracteres(nome);
+        }
+        scanner.close();
+
+    }
+}
